39be24e210743500537b07183fb5758c749daab7
Lucas Liew <lucasliew88@gmail.com>
2021-06-04 17:34:40 +0800

    deleted some redundant files?

--- Link.py
+++
@@ -1,5 +0,0 @@
-link_name = "Tele_ID"
-user_text = "Chat_ID_Links "
-url = "https://api.telegram.org/bot1530124297:AAHV94WEqPH3QwsydsmRXx65La5P-nLBWEI/getUpdates"
-
-link.xref_links.update({link_name: (user_text, url)})
