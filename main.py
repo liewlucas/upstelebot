d71f5e2ccd9d8b96865e9953309e94c9bfb2d392
YanEn <leeyanenen@gmail.com>
2021-06-04 14:19:32 +0800

    Fixed multiple scheduling??

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ def cancel(update: Update, _: CallbackContext) -> int:
     user = update.message.from_user
     logger.info("User %s canceled the conversation.", user.first_name)
     update.message.reply_text(
-        'Bye! I hope we can talk again some day.', reply_markup=ReplyKeyboardRemove()
+        'Set reminder cancelled. Hope to talk to you again. Bye!', reply_markup=ReplyKeyboardRemove()
     )
 
     return ConversationHandler.END
@@ -76,6 +76,10 @@ def timefromuser (update:Update, context: CallbackContext) -> int:
     timeresponse = R.time_response(timeusertext) # process time given under responses.py
     update.message.reply_text(timeresponse)  # first reply
     scheduletest(update, context)
+    update.message.reply_text('Type /schedule again if you want to set another reminder.')
+
+    return ConversationHandler.END
+
 
 def dayfromuser(update: Update, context: CallbackContext) -> int:
     global dayusertext
@@ -89,6 +93,7 @@ def dayfromuser(update: Update, context: CallbackContext) -> int:
 
 
 def schedule_command(update, context):
+
         reply_keyboard = [['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']]
         update.message.reply_text("Which day would you like me to send the Reminder?",
                                   reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True), )
@@ -177,7 +182,6 @@ def scheduletest(update, context):
 
 
 
-
         print("schedule set!")
 
 
@@ -219,13 +223,13 @@ def main():
             entry_points=[CommandHandler('schedule', schedule_command)],
             states={
                 DAY: [MessageHandler(Filters.regex('^(Monday|Tuesday|Wednesday|Thursday|Friday)$'), dayfromuser)],
-                TIME: [MessageHandler(Filters.regex('^([01]\d|2[0-3]):([0-5]\d)$'), timefromuser)], },
+                TIME: [MessageHandler(Filters.regex('^([01]\d|2[0-3]):([0-5]\d)$'), timefromuser)]},
             fallbacks=[CommandHandler('cancel', cancel)],
         ))
 
         dp.add_handler(CommandHandler("start", start_command))
         dp.add_handler(CommandHandler("help", help_command))
-        # dp.add_handler(CommandHandler("schedule", schedule_command))
+        #dp.add_handler(CommandHandler("schedule", schedule_command))
         dp.add_handler(conv_handler)
         dp.add_handler(CommandHandler("list", list_command))
         dp.add_handler(CommandHandler("apple", scheduletest))
