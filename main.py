ca62aff5c21bc1bb080007dff93332ac553dc1fa
Lucas Liew <lucasliew88@gmail.com>
2021-09-24 17:50:34 +0800

    Updated Welcome Message

--- main.py
+++ main.py
@@ -25,8 +25,7 @@ EDITCON, EDITINDB, EDITCHOICE, EDIT, GRP, DELETE, NAME, DAY, TIME, MESSAGE = ran
 
 def start_command(update, context):
     update.message.reply_text("Welcome to the Parakeet! \U0001F917")
-    update.message.reply_text("To get started, simply type /help to view all the operational commands\U0001F4C4")
-
+    update.message.reply_text("Parakeet is a Bot to help you with your scheduling needs! A simple registration is all it takes to do so. \n\nTo get started, simply type /help to view all operational commands \U0001F4C4")
 
 def register_command(update, context):
     global duplicatevalue
@@ -93,7 +92,8 @@ def register_command(update, context):
 
 def help_command(update, context):
     update.message.reply_text("This bot enables you to create, delete, and edit reminders. Please follow the commands stated below to get started! \U0001f60A \n\n"
-                              "/schedule is to set a new reminder\n"
+                              "/register to register as a user to set reminders in that chat!\n"
+                              "/schedule to set a new reminder\n"
                               "/list shows you a list of reminders that you have set\n"
                               "/delete allows for you to delete reminders based on the Reminder Names\n"
                               "/edit allows for you to edit existing reminders")
